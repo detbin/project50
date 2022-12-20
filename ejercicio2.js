b7dc941400c796c1c64c0c872b8f2942e41cb480
detbin <116663162+detbin@users.noreply.github.com>
2022-12-20 12:27:45 +0100

    Add files via upload

---
+++ ejercicio2.js
@@ -0,0 +1,7 @@
+const fecha= new Date ('July 14 1969');
+const day = fecha.getDate();
+const month = fecha.getMonth() + 1;
+const year = fecha.getFullYear();
+const superResult = String(day) + "/" + String(month) + "/" + String(year);
+const Datos= new Array("David",53,eresdesarrollador=true,superResult,'El Perfume');
+console.log(Datos);
